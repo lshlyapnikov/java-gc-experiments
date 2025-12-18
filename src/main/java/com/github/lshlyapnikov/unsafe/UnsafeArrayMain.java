efceaaaacc45fbaf06d5245dd1b64f5d71eaf183
Leonid Shlyapnikov <lshlyapnikov@gmail.com>
2025-12-18 17:28:30 -0500

    unsafe array experiments

--- src/main/java/com/github/lshlyapnikov/unsafe/UnsafeArrayMain.java
+++ src/main/java/com/github/lshlyapnikov/unsafe/UnsafeArrayMain.java
@@ -8,30 +8,27 @@ public class UnsafeArrayMain {
     private static final Unsafe U = Unsafe.getUnsafe();
 
     private static final int ABASE = U.arrayBaseOffset(Long[].class);
-    private static final int ASHIFT;
+    private static final int ASHIFT = calculateAshift(Long[].class);
 
-    static {
-        int scale = U.arrayIndexScale(Long[].class);
-        if ((scale & (scale - 1)) != 0)
+    private static int calculateAshift(Class<?> arrayClass) {
+        int scale = U.arrayIndexScale(arrayClass);
+        if ((scale & (scale - 1)) != 0) {
             throw new ExceptionInInitializerError("array index scale not a power of two");
-        ASHIFT = 31 - Integer.numberOfLeadingZeros(scale);
+        }
+        return 31 - Integer.numberOfLeadingZeros(scale);
     }
 
     public static void main(String[] args) {
         System.out.printf("ABASE: %s\n", ABASE);
         System.out.printf("ASHIFT: %s\n", ASHIFT);
 
-        Long[] array = new Long[32];
-
+        final Long[] array = new Long[32];
         Arrays.fill(array, 0L);
-
         System.out.printf("array: %s\n", Arrays.toString(array));
-
         for (int i = 0; i < array.length; i++) {
-            System.out.printf("offset: %s\n", ((long)i << ASHIFT));
-            U.compareAndSetReference(array, ((long)i << ASHIFT) + ABASE, 0L, (long) i);
+//            System.out.printf("offset: %s\n", ((long) i << ASHIFT));
+            U.compareAndSetReference(array, ((long) i << ASHIFT) + ABASE, 0L, (long) i);
         }
-
         System.out.printf("array: %s\n", Arrays.toString(array));
     }
 }
