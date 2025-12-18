909adc2502fb76b8ab1c049a64e64856aa9f79e1
Leonid Shlyapnikov <lshlyapnikov@gmail.com>
2025-12-18 17:16:00 -0500

    unsafe array experiments

---
+++ src/main/java/com/github/lshlyapnikov/unsafe/UnsafeArrayMain.java
@@ -0,0 +1,37 @@
+package com.github.lshlyapnikov.unsafe;
+
+import jdk.internal.misc.Unsafe;
+
+import java.util.Arrays;
+
+public class UnsafeArrayMain {
+    private static final Unsafe U = Unsafe.getUnsafe();
+
+    private static final int ABASE = U.arrayBaseOffset(Long[].class);
+    private static final int ASHIFT;
+
+    static {
+        int scale = U.arrayIndexScale(Long[].class);
+        if ((scale & (scale - 1)) != 0)
+            throw new ExceptionInInitializerError("array index scale not a power of two");
+        ASHIFT = 31 - Integer.numberOfLeadingZeros(scale);
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("ABASE: %s\n", ABASE);
+        System.out.printf("ASHIFT: %s\n", ASHIFT);
+
+        Long[] array = new Long[32];
+
+        Arrays.fill(array, 0L);
+
+        System.out.printf("array: %s\n", Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("index: %s\n", ((long)i << ASHIFT));
+            U.compareAndSetReference(array, ((long)i << ASHIFT) + ABASE, 0L, (long) i);
+        }
+
+        System.out.printf("array: %s\n", Arrays.toString(array));
+    }
+}
